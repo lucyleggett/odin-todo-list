b79baee84c8a23810c80c3d6778eac92b648caeb
lucyleggett <lucy.leggett@hotmail.com>
2026-06-18 12:49:14 +0800

    Create project files

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+import path from "node:path";
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+export default {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(import.meta.dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      inject: 'body',
+      scriptLoading: 'defer',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        use: [{
+                loader: "html-loader",
+                options: {
+                    sources: {
+                        urlFilter: (attribute, value) => {
+                            return !value.startsWith("http");
+                        }
+                    }
+                }
+            }],
+      },
+      {
+      test: /\.(png|svg|jpg|jpeg|gif)$/i,
+      type: 'asset/resource',
+      },
+    ],
+  },
+};
