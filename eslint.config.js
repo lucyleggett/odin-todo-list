7e59824466dbcff86afa3633aef5fdd30ab241d4
lucyleggett <lucy.leggett@hotmail.com>
2026-07-08 14:56:56 +0800

    Run linter

---
+++ eslint.config.js
@@ -0,0 +1,7 @@
+import css from "@eslint/css";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  { ignores: ["**/*.js", "**/*.cjs", "**/*.mjs"] },
+  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+]);
